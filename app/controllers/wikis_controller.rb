89f0b5a2b21f69a62f9842a1ec1038272027d2be
joelramsey <joel.d.ramsey@gmail.com>
2015-04-16 22:57:11 +0000

    Added ability to create and edit wikis

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,15 +1,54 @@
 class WikisController < ApplicationController
-  def index
+  def index   
     @wikis = Wiki.all
   end
 
   def show
+    @user = current_user
     @wiki = Wiki.find(params[:id])
   end
 
   def new
+    @user = current_user
+    @wiki = Wiki.new
   end
+  
+  def create
+    @user = current_user
+    @wiki = current_user.wikis.build(wiki_params)
+   
+     if @wiki.save
+       flash[:notice] = "Wiki was saved."
+       redirect_to @wiki
+     else
+       flash[:error] = "There was an error saving your wiki. Please try again."
+       render :new
+     end
+   end
 
   def edit
+    @user = current_user
+    @wiki = Wiki.find(params[:id])
   end
+  
+  def update
+    @user = current_user
+    @wiki = Wiki.find(params[:id])
+       
+    if @wiki.update_attributes(wiki_params)
+      flash[:notice] = "Wiki was successfully updated."
+      redirect_to action: 'index'
+      
+    else
+      flash[:alert] = "Error updating wiki."
+      redirect_to action: 'index'
+    end
+  end
+  
+  
+  private
+  
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+   end
 end
