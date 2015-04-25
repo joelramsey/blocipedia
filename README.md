d635a8081a7e0f1cfe6853721b5bf6ee14d9e91c
joelramsey <joel.d.ramsey@gmail.com>
2015-04-25 22:19:58 +0000

    Modified README

---
+++ README.md
@@ -0,0 +1,7 @@
+== README
+
+## Blocipedia: SaaS Wiki Collaboration Tool app that allows users to create their own wikis.
+
+Made with my mentor at [Bloc](http://bloc.io).
+
+Added to Heroku as url = https://shielded-dusk-5484.herokuapp.com/
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'joel.d.ramsey@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -257,5 +257,4 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   
-  config.mailer_sender = "joel.d.ramsey@gmail.com"
-end
+  end
