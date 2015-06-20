f9ed1befde758c9476392c17ac2b28b8031a05f2
joelramsey <joel.d.ramsey@gmail.com>
2015-06-20 02:18:42 +0000

    modified url for production mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'shielded-dusk-5484.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'hidden-wave-6157.herokuapp.com' }
   
 end
